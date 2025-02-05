31ed6981ab2f16107dac747685be81a4e9709623
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2025-02-05 12:08:58 +0600

    Create 1.Redux - Read Operation Book Crud.js

---
+++ Redux/Redux Book Crud/1.Redux - Read Operation Book Crud.js
@@ -0,0 +1,152 @@
+#Redux Book Crud without Thunk (Read Operation): 
+-------------------------------
+>>>npm create vite@latest
+
+#select : react > javascript > project_name
+
+>>> npm install
+>>> npm run dev
+
+
+================================= install react-redux and bootstap ==================================================
+>>> npm install @reduxjs/toolkit react-redux
+>>> npm install bootstrap
+
+
+
+
+
+============================================================ BookSlice ================================================
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const initialState = {
+    books: [
+        {
+            id: 1, 
+            title: 'The Alchemist',
+            author: 'Paulo Coelho',
+            price: 10,
+            quantity: 1
+        },
+         {
+            id: 2, 
+            title: 'The Name Of Jungle',
+            author: 'Momen',
+            price: 20,
+            quantity: 1
+        },
+        {
+            id: 2, 
+            title: 'Life of Pi',
+            author: 'Yann Martel',
+            price: 30,
+            quantity: 1
+        },
+    ],
+}
+
+
+export const bookSlice = createSlice({
+
+    name: 'book',
+
+    initialState: initialState,
+
+
+
+    reducers: {
+        addBook: (state, action) => {
+            state.books.push(action.payload);
+        },
+    },
+});
+
+export const { addBook } = bookSlice.actions;
+
+export default bookSlice.reducer;
+
+============================================================ app / Store.js ================================================
+
+import { configureStore } from '@reduxjs/toolkit'; 
+import BookSlice  from '../features/bookSlice';
+
+export const store = configureStore({
+
+    reducer: {
+        bookR: BookSlice,
+    },
+});
+
+
+
+============================================================ main.jsx ================================================
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+
+
+//custom import : _____________________________-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { store } from './app/store'
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
+
+
+============================================================ App.jsx ================================================
+import React from 'react'
+import BookList from './components/BookList'
+
+
+const App = () => {
+  return (
+    <div>
+      <BookList />
+    </div>
+  )
+}
+
+export default App
+================================================== BookList ======================================================
+import React from "react";
+import { useSelector } from "react-redux";
+
+const BookList = () => {
+  const books = useSelector((state) => state.bookR.books);
+  return (
+    <div>
+      <div className="row">
+        <h2 className="text-center">List of Books</h2>
+        <hr />
+        {books && books.length > 0 ? (
+          books.map((book) => {
+            return (
+              <div className="row text-center">
+                <ul>
+                  <li>
+                    <h3>
+                      {book.title} <span>{book.author}</span>
+                    </h3>
+                    <p>
+                      Price : {book.price} Tk - Quantity : {book.quantity} {""}
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            );
+          })
+        ) : (
+          <h3 className="text-center">NO Books Found</h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BookList;
