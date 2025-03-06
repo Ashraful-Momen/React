8ab038b6097dd3205423f5fa30479fa2bd910036
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2025-03-06 09:56:06 +0600

    Create 4. Add Bootstrap .jsx

---
+++ Problem Solve/4. Add Bootstrap .jsx
@@ -0,0 +1,18 @@
+#Redux Book Crud without Thunk (Read Operation): 
+-------------------------------
+>>>npm create vite@latest
+
+#select : react > javascript > project_name
+
+>>> npm install
+>>> npm run dev
+
+
+================================= install react-redux and bootstap ==================================================
+>>> npm install @reduxjs/toolkit react-redux
+>>> npm install bootstrap
+
+================================== Imort Bootstrap 5 in Jsx==============================
+//in the file import => 
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JS
