9c314e5e57140ae7f0414c04410fe0a34a9190e3
Md.Ashraful Momen <34467735+Ashraful-Momen@users.noreply.github.com>
2025-09-08 12:13:42 +0600

    Create 1. src > App.js

---
+++ Redux/Redux Thunk/My Notes /project code/1. src > App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import PostView from "./feature/PostView";
+
+function App() {
+  return (
+    <div>
+      <PostView />
+    </div>
+  );
+}
+
+export default App;
